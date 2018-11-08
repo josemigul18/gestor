793c62259a94858529e9358b1314d5f712ed4321
cunha <ze_mmc@live.com.pt>
2018-11-08 22:12:41 +0000

    final

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/cunha/Desktop/gestor/venv"
+set -gx VIRTUAL_ENV "/home/cunha/Desktop/gestor/gestor/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/cunha/Desktop/gestor/venv/bin/python3
+#!/home/cunha/Desktop/gestor/gestor/venv/bin/python3
 from django.core import management
 
 if __name__ == "__main__":
